025a2c28763571621a36ff94b08f582e71bd655b
Nguyen Duc Phu <Nguyen Duc Phu>
2016-08-26 17:39:04 +0700

    update code

---
+++ public/controllers/controller.js
@@ -0,0 +1,42 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
+	console.log("Hello controller");
+
+	var refresh = function(){
+			$http.get('/contactlist').success(function(response){
+			$scope.contactlist = response;
+			$scope.contact = "";
+		});
+	};
+
+	refresh();
+
+	$scope.addContact = function(){
+		$http.post('/contactlist', $scope.contact).success(function(response){
+			refresh();
+		});
+	};
+
+	$scope.remove = function(id) {
+	$http.delete('/contactlist/'+ id).success(function(response){
+		refresh();
+	});
+};
+
+	$scope.edit = function(id){
+		$http.get('/contactlist/' + id).success(function(response){
+			$scope.contact = response;
+		});
+};
+/*
+	$scope.update = function(){
+		$http.put('/contact/' + $scope.contact._id, $scope.contact).success(function(response){
+			refresh();
+		});
+	};
+
+	$scope.clear = function(){
+		$scope.contact = "";
+	};
+	*/
+}]);
